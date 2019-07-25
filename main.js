c795c84109ae32e233575c403cb89d69dd330f56
Hunter Nakagawa <hunternakagawa@gmail.com>
2019-07-24 22:16:10 -1000

    added index fetcher logic

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const electron = require('electron');
 //const BrowserWindow = electron.BrowserWindow
 const { app, BrowserWindow } = require('electron')
 
+
+
 // Keep a global reference of the window object, if you don't, the window will
 // be closed automatically when the JavaScript object is garbage collected.
 let mainWindow
@@ -19,9 +21,10 @@ function createWindow () {
   //mainWindow.removeMenu()
   //==================================================
 
-//webPreferences: {
-//devTools: false
-//}
+  webPreferences: {
+    devTools: true
+    nodeIntegration: true
+  }
 
   // and load the index.html of the app.
   mainWindow.loadURL(`file://${__dirname}/index.html`)
@@ -62,3 +65,5 @@ app.on('activate', function () {
 
 // In this file you can include the rest of your app's specific main process
 // code. You can also put them in separate files and require them here.
+console.log("Running extra files...")
+require("./app/getindex.js")

a1c8b19a2ed23b4bb7a8a0ec8f362898d945b459
Nicole Rappe <cyberstrawberry101@gmail.com>
2020-12-21 01:13:56 -0700

    Implemented Basic Client/Server Networking

--- Borealis/Program.cs
+++ Borealis/Program.cs
@@ -8,6 +8,10 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using System.Text;
+using System.Net.Sockets;
+using System.Net;
+using Newtonsoft.Json;
 
 namespace Borealis
 {
@@ -15,10 +19,73 @@ namespace Borealis
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            //THIS CREATES THE WEBSITE FRONT-END
+            //CreateHostBuilder(args).Build().Run();  
+
+            //FIGURE OUT HOW TO RUN BOTH OF THESE SIMULTANIOUSLY
+
+            //THIS HANDLES CREATING THE DATA TRANSFER SYSTEM BETWEEN THE AGENT AND THE CONTROL PANEL
+            IPEndPoint ep = new IPEndPoint(IPAddress.Loopback, 1234);
+            TcpListener listener = new TcpListener(ep);
+            listener.Start();
+
+            Console.WriteLine("Borealis Server Manager - Listening for Requests at {0}:{1}", ep.Address, ep.Port);
+
+            // Run the loop continously; this is the server.
+            while (true)
+            {
+                const int bytesize = 1024 * 1024;
+
+                string message = null;
+                byte[] buffer = new byte[bytesize];
+
+                var sender = listener.AcceptTcpClient();
+                sender.GetStream().Read(buffer, 0, bytesize);
+
+                // Read the message, and perform different actions
+                message = cleanMessage(buffer);
+
+                // Save the data sent by the client;
+                Person agent = JsonConvert.DeserializeObject<Person>(message); // Deserialize
+                byte[] bytes = System.Text.Encoding.Unicode.GetBytes("[SERVER RESPONSE] Payload Received from " + agent.Hostname);
+                sender.GetStream().Write(bytes, 0, bytes.Length); // Send the response
+                Console.WriteLine("[AGENT: {0} | IP ADDRESS: {1}] | Payload = {2} bytes | JSON_JATA01: {3}, JSON_JATA02: {4}, JSON_JATA03: {5}", agent.Hostname, agent.Agent_IP, bytes.Length, agent.JSON_DATA01, agent.JSON_DATA02, agent.JSON_DATA03);
+            }
         }
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
+        private static string cleanMessage(byte[] bytes)
+        {
+            string message = System.Text.Encoding.Unicode.GetString(bytes);
+
+            string messageToPrint = null;
+            foreach (var nullChar in message)
+            {
+                if (nullChar != '\0')
+                {
+                    messageToPrint += nullChar;
+                }
+            }
+            return messageToPrint;
+        }
+
+        // Sends the message string using the bytes provided.
+        private static void sendMessage(byte[] bytes, TcpClient client)
+        {
+            client.GetStream()
+                .Write(bytes, 0,
+                bytes.Length); // Send the stream
+        }
+
+    class Person
+    {
+        public string Hostname { get; set; }
+        public string Agent_IP { get; set; }
+        public string JSON_DATA01 { get; set; }
+        public string JSON_DATA02 { get; set; }
+        public string JSON_DATA03 { get; set; }
+    }
+
+    public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {

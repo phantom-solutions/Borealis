dd3699d33911f27d0e394d0c3a89d81afc2e7ee9
Nicole Rappe <cyberstrawberry101@gmail.com>
2021-01-13 21:58:06 -0700

    Test

--- Borealis/API/API_Controller.cs
+++ Borealis/API/API_Controller.cs
@@ -6,7 +6,7 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
-
+//
 namespace Borealis.API
 {
     [Route("api/[controller]")]

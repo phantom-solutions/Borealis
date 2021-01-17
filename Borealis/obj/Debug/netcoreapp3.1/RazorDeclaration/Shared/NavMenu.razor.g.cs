07d3e0f95e8cc94785e2c7da61836080177534f4
Nicole Rappe <cyberstrawberry101@gmail.com>
2021-01-17 13:37:27 -0700

    Added SQLite capability to Control Panel

--- Borealis/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
+++ Borealis/obj/Debug/netcoreapp3.1/RazorDeclaration/Shared/NavMenu.razor.g.cs
@@ -13,63 +13,63 @@ namespace Borealis.Shared
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 1 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 2 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using Microsoft.AspNetCore.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 3 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Authorization;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 4 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 5 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 6 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 7 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 8 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using Borealis;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\_Imports.razor"
+#line 9 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\_Imports.razor"
 using Borealis.Shared;
 
 #line default
@@ -83,7 +83,7 @@ using Borealis.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 38 "C:\Users\nicole.rappe\Source\Repos\Borealis\Borealis\Shared\NavMenu.razor"
+#line 38 "C:\Users\nicole.rappe\source\repos\Borealis\Borealis\Shared\NavMenu.razor"
        
     private bool collapseNavMenu = true;
 
